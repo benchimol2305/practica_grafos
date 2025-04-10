b4ef60007317b8f67e2c6135116724e900124ddb
benchimol2305 <cesarbenchimoldavila@gamil.com>
2025-04-10 01:26:46 -0400

    se crea una funcion para buscar usuario por nombre que consiste en recorrer todos los usuarios luego comparar el nombre actual con el buscado y luegp retorna si lo encuentra o no

--- practica.cpp
+++ practica.cpp
@@ -42,6 +42,15 @@ void redimensionarUsuarios(RedSocial* rs){
     rs->capacidad= nuevaCapacidad;
 
     cout<< "la red social se agrando a una capacidad de:" << nuevaCapacidad << " usuarios\n";
+
     
-    
+}
+
+int buscarUsuario(RedSocial* rs, const char* nombre){
+    for(int i=0; i<rs->numUsuarios; i++){
+        if(strcmp(rs->usuarios[i].nombre, nombre)==0){
+            return i;
+        }
+    }
+    return -1;
 }
